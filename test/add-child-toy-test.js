885057df1a52833ff1fa226d08d36bccb40fa4a6
Jufebrown <jufebrown@gmail.com>
2017-03-14 15:39:52 -0500

    wrote test to see if add-child-toy.js is a function

--- test/add-child-toy-test.js
+++ test/add-child-toy-test.js
@@ -1,1 +1,10 @@
 `use strict`
+
+const {assert: {isFunction, equal}} = require(`chai`)
+const add_child_toy = require(`../lib/add-child-toy`)
+
+describe(`add-child-toy`, () => {
+  it(`should be a function`, () => {
+    isFunction(add_child_toy)
+  })
+})
