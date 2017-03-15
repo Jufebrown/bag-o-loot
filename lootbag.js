f03d0b09aac04b2c30c4f851bf5271e87906ab63
Jufebrown <jufebrown@gmail.com>
2017-03-15 15:18:20 -0500

    demodularized it again following Joe's lecture

--- lootbag.js
+++ lootbag.js
@@ -45,3 +45,35 @@ db.run(`CREATE TABLE IF NOT EXISTS child_toys (
   FOREIGN KEY(child_id) REFERENCES children(child_id),
   FOREIGN KEY(toy_id) REFERENCES toys(toy_id)
 )`)
+
+module.exports = (db, toy, child) => {
+  let childId
+  let toyId
+
+  db.run(`INSERT INTO children VALUES (
+    null,
+    '${child}'
+  )`)
+
+  db.each(`SELECT * FROM children ORDER BY child_id DESC LIMIT 1`, (err, row) => {
+    childId = row.child_id
+    console.log('childId:', childId);
+  })
+
+  db.run(`INSERT INTO toys VALUES (
+    null,
+    '${toy}'
+  )`)
+
+  db.each(`SELECT * FROM toys ORDER BY toy_id DESC LIMIT 1`, (err, row) => {
+    toyId = row.toy_id
+    console.log('toyId:', toyId);
+  })
+
+  db.run(`INSERT INTO child_toys VALUES (
+    null,
+    ${childId},
+    ${toyId},
+    -1
+  )`)
+}
