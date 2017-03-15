c7dbaf8c69fc4a3526c1e7ea5baf236f16e0d7bb
Jufebrown <jufebrown@gmail.com>
2017-03-15 15:06:11 -0500

    made lootbag.js executable

--- test/add-child-toy-test.js
+++ test/add-child-toy-test.js
@@ -2,6 +2,8 @@
 
 const {assert: {isFunction, equal}} = require(`chai`)
 const add_child_toy = require(`../lib/add-child-toy`)
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('lootbag.sqlite', (err) => console.log('Connected'))
 
 describe(`add-child-toy`, () => {
   it(`should be a function`, () => {
@@ -11,4 +13,7 @@ describe(`add-child-toy`, () => {
   it(`should take 3 arguments`, () => {
     equal(add_child_toy.length, 3)
   })
+
+
+  add_child_toy(db, 'BB Gun', 'Sam')
 })
